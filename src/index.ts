959b1254504505f657f0e78b16bdb916ca7e7fb2
Shivananda Sai <shivanandasai.38@gmail.com>
2025-12-04 20:06:52 +0530

    Add initial project setup with TypeScript, WebSocket server, and package configuration

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on("connection", (socket) => {
+  allSockets.push(socket);
+  userCount++;
+  console.log("user connected #", userCount);
+
+  socket.on("message", (message) => {
+    console.log("message received", message.toString());
+
+    allSockets.forEach((s) => {
+      s.send(message.toString());
+    });
+  });
+});
